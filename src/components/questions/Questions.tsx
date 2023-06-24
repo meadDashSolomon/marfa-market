93b12d9f567c833ab689008d285b84414759ed56
Jung Hoan Kim <kjunghoan@gmail.com>
2023-06-24 18:31:18 -0400

    implement get request with default values & add typescript for node

--- src/components/questions/Questions.tsx
+++ src/components/questions/Questions.tsx
@@ -1,17 +1,36 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
+import { useState, useEffect } from "react";
 // import { Button } from '@mui/material';
 
-import QuestionList from "./QuestionList"
-import SearchBar from "./SearchBar"
+import QuestionList from "./QuestionList";
+import SearchBar from "./SearchBar";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
+import axios from "axios";
+import getRequest from './request';
 
 
 export default function Questions() {
+  const[questions,setQuestions] = useState();
+
+  const getQuestions =()=>{
+    return axios.request(getRequest())
+    .then((response) => {
+      console.log(response);
+      setQuestions(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  };//end of getQuestions
+
+  useEffect(() => {
+    getQuestions();
+  },[]);
   return (
     <div>
       <h3>QUESTIONS & ANSWERS</h3>
       <SearchBar/>
-      <QuestionList/>
+      <QuestionList questions = {questions}/>
 
     </div>
   );
