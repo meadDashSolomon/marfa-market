9553135b64324bc91ecf2ca407a1c10c1a96da29
Jimmie <legendaryprophecy@gmail.com>
2023-06-23 20:28:39 -0400

    Minor cosmetic changes

--- src/components/related/Item.tsx
+++ src/components/related/Item.tsx
@@ -7,12 +7,12 @@ function Item( { item } ) {
   const [ rating, setRating ] = useState(3.5);
 
   return (
-    <Card>
+    <Card sx={{padding: '10px'}}>
       <div className='item-card'>
       <img src={item.results ? item.results[0].photos[0].thumbnail_url : ''} style={{width: '100%', height: '150px', objectFit: 'cover'}}/>
-      <Typography> { item.category } </Typography>
-      <Typography> { item.name } </Typography>
-      <Typography> { item.default_price } </Typography>
+      <Typography variant='h8' sx={{color: '#124559'}}> { item.category } </Typography>
+      <Typography variant='h6' sx={{color: '#124559'}}> { item.name } </Typography>
+      <Typography sx={{color: '#F6AE2D'}}> { item.default_price } </Typography>
       <Rating
         name="text-feedback"
         value={rating}
