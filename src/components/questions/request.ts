7ed9a9e53e6411782165fca0f5ec860f2b3ea77e
Jung Hoan Kim <125612022+kjunghoan@users.noreply.github.com>
2023-06-26 19:09:02 -0400

    Update request.ts

--- src/components/questions/request.ts
+++ src/components/questions/request.ts
@@ -1,6 +1,7 @@
 
 export default function getRequest (productNum = '37314', page = 1, count = 5) {
     return {
+      method: 'GET',
       url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/qa/questions',
       params: {
         product_id: `${productNum}`,
