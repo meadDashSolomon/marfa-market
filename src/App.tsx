8641df7700c8fdc0b47190535c716fd088496594
Mead Solomon <mead93@gmail.com>
2023-06-26 15:48:41 -0500

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-// @ts-ignore
-import React from 'react';
+import { useState, useEffect }  from 'react';
 import Overview from './components/overview/Overview';
 import Questions from './components/questions/Questions';
 import Ratings from './components/ratings/Ratings';
 import Related from './components/related/Related';
+import axios from 'axios';
 
 const App = () => {
+  const [currentItem, setCurrentItem] = useState({});
+
+  useEffect(() => {
+    // axios.get()
+    console.log(typeof import.meta.env.VITE_AUTH_TOKEN);
+  }, [])
+
   return (
     <div>
       <Overview  />
