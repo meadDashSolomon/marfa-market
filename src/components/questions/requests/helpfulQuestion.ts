45e01a36f643757e89ab4f2198f03cbabd98a174
Jung Hoan Kim <kjunghoan@gmail.com>
2023-07-03 13:34:28 -0400

    Corrected put requests for report & helpful

--- src/components/questions/requests/helpfulQuestion.ts
+++ src/components/questions/requests/helpfulQuestion.ts
@@ -2,9 +2,6 @@ export default function helpfulQuestion (question_id:number) {
   return {
     method:'PUT',
     url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/qa/questions/${question_id}/helpful`,
-    article: {
-
-    },
     headers: {
             Authorization: import.meta.env.VITE_AUTH_TOKEN
     }
--- src/components/questions/requests/reportQuestion.ts
+++ src/components/questions/requests/reportQuestion.ts
@@ -2,9 +2,6 @@ export default function reportQuestion(question_id:number) {
   return {
     method:'PUT',
     url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/qa/questions/${question_id}/report`,
-    article: {
-
-    },
     headers: {
             Authorization: import.meta.env.VITE_AUTH_TOKEN
     }
