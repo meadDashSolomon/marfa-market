489f1b817a55e21b028b22843a3bd95f28ea07c0
Jung Hoan Kim <kjunghoan@gmail.com>
2023-07-06 02:06:51 -0400

    Finish AnswerListEntry

--- src/components/questions/answerComponents/AnswerListEntry.tsx
+++ src/components/questions/answerComponents/AnswerListEntry.tsx
@@ -1,54 +1,86 @@
 // import { useState, useEffect } from "react";
 
-import { Card, Stack, Typography } from "@mui/joy"
-import { format } from "date-fns"
-
+import helpfulAnswer from "../requests/helpfulAnswer";
+import { Card, Stack, Typography } from "@mui/joy";
+import { Button } from "@mui/material";
+import { format } from "date-fns";
+import { useState } from "react";
+import axios from "axios";
 
 type AnswerListEntryProps = {
-    answer: {
-        answer_id: number,
-        body: string,
-        date: string,
-        answerer_name: string,
-        helpfulness: number,
-        photos: string[]
-    };
-}
+  answer: {
+    answer_id: number;
+    body: string;
+    date: string;
+    answerer_name: string;
+    helpfulness: number;
+    photos: string[];
+  };
+};
 
 export default function AnswerListEntry(props: AnswerListEntryProps) {
-    const validPhotoUrl = new RegExp('^(http(s?):)([/.\\w\\s-])*.(?:jpg|gif|png)', 'i')
+  const validPhotoUrl = new RegExp(
+    "^(http(s?):)([/.\\w\\s-])*.(?:jpg|gif|png)",
+    "i"
+  );
+  const [helpful, setHelpful] = useState<string>("Helpful?");
 
-    // console.log(props.answer.body)
+  const displayUserName = () => {
+    // if the userName of the Seller is the same as the userName of the answerer but it in bold
+    return props.answer.answerer_name === "Seller" ? (
+      <strong>{props.answer.answerer_name}</strong>
+    ) : (
+      props.answer.answerer_name
+    );
+  };
 
-    const displayUserName = () => {
-        // if the userName of the Seller is the same as the userName of the answerer but it in bold 
-        return props.answer.answerer_name === 'Seller' ? <strong>{props.answer.answerer_name}</strong>: props.answer.answerer_name;
+  const mappedImages = () => {
+    //refactor to somewhat match what the overview pictures do
+    return props.answer.photos.map((photo) => {
+      if (validPhotoUrl.test(photo)) {
+        return (
+          <img
+            key={props.answer.photos.indexOf(photo)}
+            src={photo}
+            style={{ maxHeight: "200px" }}
+            onClick={() => {
+              alert("this needs to be added but should expand the image");
+            }}
+          />
+        );
+      }
+    });
+  };
+  const answerHelpful = () => {
+    if (helpful === "Helpful?") {
+      axios
+        .request(helpfulAnswer(props.answer.answer_id))
+        .then((response) => console.log(response.status, response.statusText));
+      setHelpful("Helpful");
+    } else {
+      console.log("This was already triggered");
     }
+  };
 
-    const mappedImages = () => { //refactor to somewhat match what the overview pictures do
-        return (props.answer.photos).map((photo) => {
-            if (validPhotoUrl.test(photo)) {
-                return (<img
-                    key={props.answer.photos.indexOf(photo)}
-                    src={photo}
-                    style={{maxHeight: '200px'}}
-                    onClick={()=> {
-                        alert('this needs to be added but should expand the image')
-                    }}
-                />);
-            }
-        })
-    }
-
-    return (
-        <Card className='Questions AnswerListEntry'>
-            <Typography level="h5"><strong>A:</strong>{props.answer.body}</Typography>
-            <Typography
-            level="body3"
-            >by {displayUserName()}, {format(new Date(props.answer.date),"PPP")} | Helpful? <a>yes</a>({props.answer.helpfulness}) | <a>report</a></Typography>
-                <Stack direction={'row'}>
-                    {mappedImages()}
-                </Stack>
-        </Card>
-    )
+  return (
+    <Card className="Questions AnswerListEntry">
+      <Typography level="h5">
+        <strong>A:</strong>
+        {props.answer.body}
+      </Typography>
+      <Typography level="body3">
+        by {displayUserName()},{format(new Date(props.answer.date), "PPP")}|{" "}
+        {helpful}{" "}
+        <Button
+          onClick={() => {
+            answerHelpful();
+          }}
+        >
+          yes
+        </Button>
+        {props.answer.helpfulness} | <Button>report</Button>
+      </Typography>
+      <Stack direction={"row"}>{mappedImages()}</Stack>
+    </Card>
+  );
 }
--- src/components/questions/questionComponents/QuestionListEntry.tsx
+++ src/components/questions/questionComponents/QuestionListEntry.tsx
@@ -1,6 +1,6 @@
 import { Button, Card } from "@mui/material";
 import AnswerList from "../answerComponents/AnswerList";
-import { format } from "date-fns"
+import { format } from "date-fns";
 import { Typography } from "@mui/joy";
 import { useCallback, useEffect, useState } from "react";
 import AnswerModal from "../answerComponents/AnswerModal";
@@ -9,120 +9,140 @@ import axios from "axios";
 import getAnswers from "../requests/getAnswers";
 
 type QuestionListEntryProps = {
-    searchQuery?:string;
-    key:number,
-    question:{
-        question_id:number,
-        question_body:string,
-        question_date:string,
-        asker_name:string,
-        question_helpfulness:number,
-        reported:boolean,
-        answers: object
-    }
-}
+  searchQuery?: string;
+  key: number;
+  question: {
+    question_id: number;
+    question_body: string;
+    question_date: string;
+    asker_name: string;
+    question_helpfulness: number;
+    reported: boolean;
+    answers: object;
+  };
+};
 
 type answersType = {
-    answer_id: number;
-    body: string;
-    date: string;
-    answerer_name: string;
-    helpfulness: number;
-    photos: string[];
-}[]
-export default function QuestionListEntry(props:QuestionListEntryProps) {
-    //New Answer Modal Components
-    const[answerModal, setAnswerModal] = useState<boolean>(false);
-    const renderAnswerModal = useCallback(() => { //conditial rendering of AnswerModal
-        if(answerModal) {
-            return (
-                <AnswerModal
-                question_id={props.question.question_id}
-                answerModal = {answerModal}
-                setAnswerModal={setAnswerModal}
-                />
-            );
-        }
-    },[answerModal, props.question.question_id]);
-
-
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    const[numAnswers, setNumAnswers] = useState<number>(2)//how many answers are being mapped
-
-    //Getting the Answers for Question [and caching]
-    const listOfAnswers:answersType = [];
-    const[answers, setAnswers] = useState<answersType>([]);
-    const answersFromSeller:answersType = []
-    useEffect(()=> {
-        axios.request(getAnswers(props.question.question_id))
-        .then((response) => {
-
-            if(response.data.results.length > 0)console.log('this is what we got back in answer',response.data.results);
-            for(const el of response.data.results) {
-                if(el.answerer_name === 'Seller') {
-                    answersFromSeller.push(el);
-                } else if(!listOfAnswers.includes(el)) {
-                    listOfAnswers.push(el);
-                }
-            }
-            answersFromSeller.sort((a, b)=> b.helpfulness - a.helpfulness);
-            listOfAnswers.sort((a, b)=> b.helpfulness - a.helpfulness)
-            console.log(answersFromSeller.concat(listOfAnswers))
-            setAnswers(answersFromSeller.concat(listOfAnswers));
-        }).catch((error)=> console.log('ERROR IN GET ANSWERS',error));
-    },[renderAnswerModal])
-
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    //Helpful Button Components
-    const[helpful, setHelpful] = useState<string>('Helpful?');
-    const helpfulFunction = () => { // need to finish
-        if(helpful !== 'Helpful') {
-            setHelpful('Helpful');
-            helpfulRequest();
-        } else {
-            console.log('no stop that')
-        }
-    };
-    const helpfulRequest = () => {// sends that a question was helpful
-        axios.request(helpfulQuestion(props.question.question_id))
-            .then(response=> {
-                console.log(response.status)
-            })
-            .catch((error)=> console.log('error in helpful request',error))
-    };
-    const renderMoreAnswersButton = () =>{
-        if(answers.length > listOfAnswers.length) {
-            return(
-                <Button
-                    onClick={()=> {setNumAnswers(numAnswers + 2)}}
-                >LOAD MORE ANSWERS</Button>
-            )
-        }
+  answer_id: number;
+  body: string;
+  date: string;
+  answerer_name: string;
+  helpfulness: number;
+  photos: string[];
+}[];
+export default function QuestionListEntry(props: QuestionListEntryProps) {
+  //New Answer Modal Components
+  const [answerModal, setAnswerModal] = useState<boolean>(false);
+  const renderAnswerModal = useCallback(() => {
+    //conditial rendering of AnswerModal
+    if (answerModal) {
+      return (
+        <AnswerModal
+          question_id={props.question.question_id}
+          answerModal={answerModal}
+          setAnswerModal={setAnswerModal}
+        />
+      );
     }
+  }, [answerModal, props.question.question_id]);
 
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  const [numAnswers, setNumAnswers] = useState<number>(2); //how many answers are being mapped
 
-    return (
-        <Card className = "Questions QuestionList QuestionListEntry">
-            <Card>{renderAnswerModal()}</Card>
-            <Typography level = {"h4"}><strong>Q:</strong>{props.question.question_body}</Typography>
-            <Typography level = {"subtitle1"}>{props.question.asker_name}</Typography>
-            <Typography level = "subtitle2">{format(new Date(props.question.question_date),"PPP")}</Typography>
-            <Typography>{helpful}
-                <Button onClick={helpfulFunction}>  YES  </Button>
-                <small>  {props.question.question_helpfulness}  </small> | 
-                <Button 
-                    onClick={()=>{
-                        setAnswerModal(true)
-                    }}>Add Answer</Button>
-            </Typography>
-            <AnswerList
-                answers={answers.slice(0, numAnswers)}
-                numAnswers={numAnswers}
-                setNumAnswers={setNumAnswers}
-                listOfAnswers={listOfAnswers}
-                searchQuery={props.searchQuery}
-            />
-            {renderMoreAnswersButton()}
-        </Card>
-    )
+  //Getting the Answers for Question [and caching]
+  const listOfAnswers: answersType = [];
+  const [answers, setAnswers] = useState<answersType>([]);
+  const answersFromSeller: answersType = [];
+  useEffect(() => {
+    axios
+      .request(getAnswers(props.question.question_id))
+      .then((response) => {
+        if (response.data.results.length > 0)
+          console.log(
+            "this is what we got back in answer",
+            response.data.results
+          );
+        for (const el of response.data.results) {
+          if (el.answerer_name === "Seller") {
+            answersFromSeller.push(el);
+          } else if (!listOfAnswers.includes(el)) {
+            listOfAnswers.push(el);
+          }
+        }
+        answersFromSeller.sort((a, b) => b.helpfulness - a.helpfulness);
+        listOfAnswers.sort((a, b) => b.helpfulness - a.helpfulness);
+        console.log(answersFromSeller.concat(listOfAnswers));
+        setAnswers(answersFromSeller.concat(listOfAnswers));
+      })
+      .catch((error) => console.log("ERROR IN GET ANSWERS", error));
+  }, [renderAnswerModal]);
+
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  //Helpful Button Components
+  const [helpful, setHelpful] = useState<string>("Helpful?");
+  const helpfulFunction = () => {
+    // need to finish
+    if (helpful !== "Helpful") {
+      setHelpful("Helpful");
+      helpfulRequest();
+    } else {
+      console.log("no stop that");
+    }
+  };
+  const helpfulRequest = () => {
+    // sends that a question was helpful
+    axios
+      .request(helpfulQuestion(props.question.question_id))
+      .then((response) => {
+        console.log(response.status);
+      })
+      .catch((error) => console.log("error in helpful request", error));
+  };
+  const renderMoreAnswersButton = () => {
+    if (answers.length > listOfAnswers.length) {
+      return (
+        <Button
+          onClick={() => {
+            setNumAnswers(numAnswers + 2);
+          }}
+        >
+          LOAD MORE ANSWERS
+        </Button>
+      );
+    }
+  };
+
+  return (
+    <Card className="Questions QuestionList QuestionListEntry">
+      <Card>{renderAnswerModal()}</Card>
+      <Typography level="h4">
+        <strong>Q:</strong>
+        {props.question.question_body}
+      </Typography>
+      <Typography level="subtitle1">{props.question.asker_name}</Typography>
+      <Typography level="subtitle2">
+        {format(new Date(props.question.question_date), "PPP")}
+      </Typography>
+      <Typography>
+        {helpful}
+        <Button onClick={helpfulFunction}> YES </Button>
+        <small> {props.question.question_helpfulness} </small> |
+        <Button
+          onClick={() => {
+            setAnswerModal(true);
+          }}
+        >
+          Add Answer
+        </Button>
+      </Typography>
+      <AnswerList
+        answers={answers.slice(0, numAnswers)}
+        numAnswers={numAnswers}
+        setNumAnswers={setNumAnswers}
+        listOfAnswers={listOfAnswers}
+        searchQuery={props.searchQuery}
+      />
+      {renderMoreAnswersButton()}
+    </Card>
+  );
 }
